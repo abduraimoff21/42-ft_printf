2002210f50f5fee7120a91d4d2d35cbda95303f4
Doston Abduraimov <103260411+abduraimoff21@users.noreply.github.com>
2023-07-11 15:21:33 +0400

    Update ft_print_utils.c

--- ft_printf/ft_print_utils.c
+++ ft_printf/ft_print_utils.c
@@ -25,17 +25,14 @@ int	ft_printstr(char *str)
 {
 	int	i;
 
-	i = 0;
+	i = -1;
 	if (str == NULL)
 	{
 		ft_putstr("(null)");
 		return (6);
 	}
-	while (str[i])
-	{
+	while (str[++i])
 		write(1, &str[i], 1);
-		i++;
-	}
 	return (i);
 }
 
